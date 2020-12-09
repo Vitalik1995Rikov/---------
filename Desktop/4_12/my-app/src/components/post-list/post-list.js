d5a6cab259a91a594263e63eee18e76ba3e2c40e
Vitalik1995Rikov <68601312+Vitalik1995Rikov@users.noreply.github.com>
2020-12-09 23:13:07 +0300

    2

--- Desktop/4_12/my-app/src/components/post-list/post-list.js
+++ Desktop/4_12/my-app/src/components/post-list/post-list.js
@@ -3,24 +3,12 @@ import PostListItem from '../post-list-item/post-list-item';
 
 import './post-list.css';
 
-const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
-
-    const elements = posts.map((item) =>{
-        const {id, ...itemProps} = item;
-        return (
-            <li key={id} className="list-group-item">
-                <PostListItem 
-                {...itemProps}
-                onDelete={() => onDelete(id)}
-                onToggleImportant={() => onToggleImportant(id)}
-                onToggleLiked={() => onToggleLiked(id)}/>
-            </li>
-        )
-    });
-
+const PostList = () => {
     return (
         <ul className="app-list list-group">
-            {elements}
+            <PostListItem/>
+            <PostListItem/>
+            <PostListItem/>
         </ul>
     )
 }
--- Desktop/4_12/my-app/src/components/post-status-filter/post-status-filter.js
+++ Desktop/4_12/my-app/src/components/post-status-filter/post-status-filter.js
@@ -1,34 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './post-status-filter.css';
 
-export default class PostStatusFilter extends Component {
-    constructor (props) {
-        super(props);
-        this.buttons = [
-            {name: 'all', label: 'Все'},
-            {name: 'like', label: 'Понравилось'}
-        ]
-    }
-    render () {
-        const buttons = this.buttons.map(({name, label}) => {
-            const {filter, onFilterSelect} = this.props;
-            const active = filter === name;
-            const clazz = active ? 'btn-info' : 'btn-outline-secondary';
-            return (
-                <button 
-                key={name} 
-                type="button" 
-                className={`btn ${clazz}`}
-                onClick={() => onFilterSelect(name)}
-                >{label}</button>
-            )
-        });
-        return (
-            <div className="btn-group">
-                {buttons}
-            </div>
-        )
-    }
+const PostStatusFilter = () => {
+    return (
+        <div className="btn-group">
+            <button/>
+            <button/>
+        </div>
+    )
 }
 
+export default PostStatusFilter;
